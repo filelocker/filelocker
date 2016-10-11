912a8c275d00b7e1420927be6e1cd6777c7f72ea
Christopher Miller <cmiller0189@gmail.com>
2016-10-10 21:52:57 -0500

    mass remove

--- src/Filelocker.Web/Frontend/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import {Routes, RouterModule} from "@angular/router";
-import {ModuleWithProviders} from "@angular/core";
-
-const appRoutes: Routes  = [
-    { path: "", redirectTo: "home", pathMatch: "full" },
-    { path: "product", loadChildren: "app/product/product.module#ProductModule"  }
-];
-
-export const routing: ModuleWithProviders  = RouterModule.forRoot(appRoutes);
--- src/Filelocker.Web/Frontend/app/home/home.routing.ts
+++
@@ -1,7 +0,0 @@
-import {RouterModule} from "@angular/router";
-import {ModuleWithProviders} from "@angular/core";
-import {HomeComponent} from "./home.component";
-
-export const routing: ModuleWithProviders = RouterModule.forChild([
-    { path: "home", component: HomeComponent}
-]);
