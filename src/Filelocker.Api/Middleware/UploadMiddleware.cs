de1a6a54b3c8f019c35b0c0f89e8bef7ec1df2cb
Brett Davis <hctv19@gmail.com>
2016-10-11 10:19:03 -0500

    Pulled upload middleware out to separate file and setup swagger

---
+++ src/Filelocker.Api/Middleware/UploadMiddleware.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Filelocker.Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Logging;
+
+namespace Filelocker.Api.Middleware
+{
+    public class UploadMiddleware
+    {
+        private string _route = "/api/files";
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+        private readonly IFileStorageProvider _fileStorageProvider;
+
+        public UploadMiddleware(RequestDelegate next, ILoggerFactory loggerFactory, IFileStorageProvider fileStorageProvider)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<UploadMiddleware>();
+            _fileStorageProvider = fileStorageProvider;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.HasValue && context.Request.Path.Value == _route)
+            {
+                _logger.LogInformation("Handling file upload: " + context.Request.Path);
+                if (!IsMultipartContentType(context.Request.ContentType))
+                {
+                    await _next.Invoke(context);
+                    return;
+                }
+                var bodyValues = new Dictionary<string, string>();
+                // Copy the request stream to the memory stream.
+                var boundary = GetBoundary(context.Request.ContentType);
+                var reader = new MultipartReader(boundary, context.Request.Body);
+                var section = await reader.ReadNextSectionAsync();
+
+                while (section != null)
+                {
+                    // process each image
+                    const int chunkSize = 1024;
+                    var fileBuffer = new byte[chunkSize];
+                    if (IsFileSection(section.ContentDisposition))
+                    {
+                        var fileName = GetFileName(section.ContentDisposition);
+
+                        using (var stream = _fileStorageProvider.GetStream(fileName))
+                        {
+                            var bytesRead = 0;
+                            do
+                            {
+                                bytesRead = await section.Body.ReadAsync(fileBuffer, 0, fileBuffer.Length);
+
+                                //TODO: Encrypt
+                                await stream.WriteAsync(fileBuffer, 0, bytesRead);
+                                //TODO: Update a SignalR Hub for progress tracking
+
+                            } while (bytesRead > 0);
+                        }
+                    }
+                    else // A form KVP
+                    {
+                        var kvp = await GetKeyValuePairFromFormDataAsync(section);
+                        bodyValues.Add(kvp.Key, kvp.Value);
+                    }
+
+                    section = await reader.ReadNextSectionAsync();
+                }
+                // Write response
+                await context.Response.WriteAsync("Done.");
+                
+                _logger.LogInformation("Finished file upload.");
+            }
+            await _next.Invoke(context);
+        }
+
+        private static bool IsMultipartContentType(string contentType)
+        {
+            return
+                !string.IsNullOrEmpty(contentType) &&
+                contentType.IndexOf("multipart/", StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+
+        private static string GetBoundary(string contentType)
+        {
+            var elements = contentType.Split(' ');
+            var element = elements.First(entry => entry.StartsWith("boundary="));
+            var boundary = element.Substring("boundary=".Length);
+            // Remove quotes
+            if (boundary.Length >= 2 && boundary[0] == '"' &&
+                boundary[boundary.Length - 1] == '"')
+            {
+                boundary = boundary.Substring(1, boundary.Length - 2);
+            }
+            return boundary;
+        }
+
+        private async Task<KeyValuePair<string, string>> GetKeyValuePairFromFormDataAsync(MultipartSection section)
+        {
+            string key = string.Empty;
+            string value = string.Empty;
+
+            var parts = section.ContentDisposition.Split(';');
+            if (parts.SingleOrDefault(part => part.Contains("name")) != null)
+            {
+                key = parts[1].Split('=')[1].Replace("\\", "").Replace("\"", "");
+                using (var streamReader = new StreamReader(section.Body))
+                {
+                    value = await streamReader.ReadToEndAsync();
+                }
+            }
+
+            return new KeyValuePair<string, string>(key, value);
+        }
+
+        private bool IsFileSection(string contentDisposition)
+        {
+            return contentDisposition.Split(';').SingleOrDefault(part => part.Contains("filename")) != null;
+        }
+
+        private string GetFileName(string contentDisposition)
+        {
+            return contentDisposition
+                .Split(';')
+                .SingleOrDefault(part => part.Contains("filename"))
+                .Split('=')
+                .Last()
+                .Trim('"');
+        }
+    }
+}
--- src/Filelocker.Api/Startup.cs
+++ src/Filelocker.Api/Startup.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Filelocker.Api.Middleware;
 using Filelocker.Domain.Interfaces;
 using Filelocker.FileSystemProviders;
 using Microsoft.AspNetCore.Builder;
@@ -13,6 +14,7 @@ using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.Swagger.Model;
 
 namespace Filelocker.Api
 {
@@ -35,120 +37,35 @@ namespace Filelocker.Api
         {
             // Add framework services.
             services.AddScoped<IFileStorageProvider>(p => new FileSystemStorageProvider(@"C:\Temp\filelocker"));
+            services.AddSwaggerGen();
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.SingleApiVersion(new Info
+                {
+                    Version = "v1",
+                    Title = "Geo Search API",
+                    Description = "A simple api to search using geo location in Elasticsearch",
+                    TermsOfService = "None"
+                });
+                options.DescribeAllEnumsAsStrings();
+            });
             services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IFileStorageProvider fileStorageProvider)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-            app.Use(async (context, next) =>
-            {
-                if (!IsMultipartContentType(context.Request.ContentType))
-                {
-                    await next();
-                    return;
-                }
-                var bodyValues = new Dictionary<string, string>();
-                // Copy the request stream to the memory stream.
-                var boundary = GetBoundary(context.Request.ContentType);
-                var reader = new MultipartReader(boundary, context.Request.Body);
-                var section = await reader.ReadNextSectionAsync();
 
-                while (section != null)
-                {
-                    // process each image
-                    const int chunkSize = 1024;
-                    var fileBuffer = new byte[chunkSize];
-                    if (IsFileSection(section.ContentDisposition))
-                    {
-                        var fileName = GetFileName(section.ContentDisposition);
-
-                        using (var stream = fileStorageProvider.GetStream(fileName))
-                        {
-                            var bytesRead = 0;
-                            do
-                            {
-                                bytesRead = await section.Body.ReadAsync(fileBuffer, 0, fileBuffer.Length);
-
-                                //TODO: Encrypt
-                                await stream.WriteAsync(fileBuffer, 0, bytesRead);
-                                //TODO: Update a SignalR Hub for progress tracking
-
-                            } while (bytesRead > 0);
-                        }
-                    }
-                    else // A form KVP
-                    {
-                        var kvp = await GetKeyValuePairFromFormDataAsync(section);
-                        bodyValues.Add(kvp.Key, kvp.Value);
-                    }
-
-                    section = await reader.ReadNextSectionAsync();
-                }
-
-
-                // Write response
-                await context.Response.WriteAsync("Done.");
-
-            });
+            app.UseMiddleware<UploadMiddleware>();
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUi();
         }
 
-        private static bool IsMultipartContentType(string contentType)
-        {
-            return
-                !string.IsNullOrEmpty(contentType) &&
-                contentType.IndexOf("multipart/", StringComparison.OrdinalIgnoreCase) >= 0;
-        }
-
-        private static string GetBoundary(string contentType)
-        {
-            var elements = contentType.Split(' ');
-            var element = elements.First(entry => entry.StartsWith("boundary="));
-            var boundary = element.Substring("boundary=".Length);
-            // Remove quotes
-            if (boundary.Length >= 2 && boundary[0] == '"' &&
-                boundary[boundary.Length - 1] == '"')
-            {
-                boundary = boundary.Substring(1, boundary.Length - 2);
-            }
-            return boundary;
-        }
-
-        private async Task<KeyValuePair<string, string>> GetKeyValuePairFromFormDataAsync(MultipartSection section)
-        {
-            string key = string.Empty;
-            string value = string.Empty;
-
-            var parts = section.ContentDisposition.Split(';');
-            if (parts.SingleOrDefault(part => part.Contains("name")) != null)
-            {
-                key = parts[1].Split('=')[1].Replace("\\", "").Replace("\"", "");
-                using (var streamReader = new StreamReader(section.Body))
-                {
-                    value = await streamReader.ReadToEndAsync();
-                }
-            }
-
-            return new KeyValuePair<string, string>(key, value);
-        }
-
-        private bool IsFileSection(string contentDisposition)
-        {
-            return contentDisposition.Split(';').SingleOrDefault(part => part.Contains("filename")) != null;
-        }
-
-        private string GetFileName(string contentDisposition)
-        {
-            return contentDisposition
-                .Split(';')
-                .SingleOrDefault(part => part.Contains("filename"))
-                .Split('=')
-                .Last()
-                .Trim('"');
-        }
+        
     }
 }
